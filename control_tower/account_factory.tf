a77f8edb2dc92de0ef357adadcf8d189bd0d16dc
Singh <nishant.singh@softwareone.com>
2025-07-14 13:42:14 +0800

    deploy lz

--- control_tower/account_factory.tf
+++ control_tower/account_factory.tf
@@ -4,11 +4,11 @@ module "ou" {
 }
 
 
-module "accounts" {
-  for_each          = { for acc in var.accounts : "${acc.account_name}" => acc }
-  source            = "../modules/account"
-  account_name      = each.value.account_name
-  account_email     = each.value.account_email
-  ou_name           = each.value.ou_name
-}
-
+#module "accounts" {
+#  for_each          = { for acc in var.accounts : "${acc.account_name}" => acc }
+#  source            = "../modules/account"
+#  account_name      = each.value.account_name
+#  account_email     = each.value.account_email
+#  ou_name           = each.value.ou_name
+#}
+#
